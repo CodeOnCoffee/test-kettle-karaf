4af34b1b7920fc1fcd22e62f6e306208deddb2cd
Nick Baker <codeoncoffee@gmail.com>
2016-01-26 16:28:40 -0500

    Update extra-features.py

--- extra-features.py
+++ extra-features.py
@@ -1,2 +1,2 @@
 repositories = ["mvn:pentaho/pentaho-kettle-feature/6.1-SNAPSHOT/xml/features", "mvn:io.hawt/hawtio-karaf/1.4.59/xml/features"]
-features = ["camel-scala", "camel-zipfile", "hawtio"] 
+features = ["camel-scala", "camel-zipfile", "hawtio", "pentaho-kettle-feature"] 
