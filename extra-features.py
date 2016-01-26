ede6b091349bbac0d6d4a045714b3dfde53acc44
Nick Baker <codeoncoffee@gmail.com>
2016-01-26 15:36:46 -0500

    Update extra-features.py

--- extra-features.py
+++ extra-features.py
@@ -1,2 +1,2 @@
-repositories = ["mvn:pentaho/pentaho-kettle-feature/6.1-SNAPSHOT/xml/features", "mvn:io.hawt/hawtio-karaf/1.4.59/xml/features"]
-features = ["pentaho-kettle-feature", "hawtio"]
+repositories = []
+features = []
