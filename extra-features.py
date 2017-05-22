9577f1ece0cd558c4526f01889f35f6191a48239
Nick Baker <codeoncoffee@gmail.com>
2017-05-21 21:54:34 -0400

    Update extra-features.py

--- extra-features.py
+++ extra-features.py
@@ -1,3 +1,3 @@
-repositories = ["mvn:pentaho/pentaho-kettle-feature/7.1-SNAPSHOT/xml/features", "mvn:io.hawt/hawtio-karaf/1.4.59/xml/features"]
+repositories = ["mvn:pentaho/pentaho-kettle-feature/7.1-SNAPSHOT/xml/features", "mvn:org.apache.activemq/activemq-karaf/5.10.0/xml/features", "mvn:io.hawt/hawtio-karaf/1.4.59/xml/features"]
 features = ["camel-scala", "camel-zipfile", "hawtio", "activemq-broker"]
 extra_features = ["pentaho-kettle-feature-base","pentaho-kettle-feature"] 
